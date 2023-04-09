1712cfb6485c49d44981f4eb89880b46982341a1
Yuto Nishida <yuto@berkeley.edu>
2023-04-09 03:10:44 -0700

    Format

--- pages/links.tsx
+++ pages/links.tsx
@@ -3,21 +3,17 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
-export interface YuttoProps {
-
-}
+export interface YuttoProps {}
 export default function Yutto(props: YuttoProps) {
-  return (
-    <Layout>
-      <Head>
-        <title>Yutto - {siteTitle}</title>
-        <h1
-				  style={{ textAlign: "center" }}
-				  className={`${utilStyles.heading2Xl} ${utilStyles.bold}`}
-        >
-        
-        </h1>
-      </Head>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<Head>
+				<title>Yutto - {siteTitle}</title>
+				<h1
+					style={{ textAlign: "center" }}
+					className={`${utilStyles.heading2Xl} ${utilStyles.bold}`}
+				></h1>
+			</Head>
+		</Layout>
+	);
 }
